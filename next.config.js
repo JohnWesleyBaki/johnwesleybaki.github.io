04f623490f85d86d8461dc96de0b2cf0358808e8
John Wesley Baki <johnwesleybaki3@gmail.com>
2024-09-03 20:07:05 +0530

    added caching for the builds-3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      forceSwcTransforms: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  output: 'export', 
+};
+
+module.exports = nextConfig;
